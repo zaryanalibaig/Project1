a0dec6e47fc5150020e81b596b0b0e7849e31352
Zaryan <zaryan.ali.baig@gmail.com>
2026-05-08 17:23:33 +0500

    1,2 add

--- Project1/pr.cpp
+++ Project1/pr.cpp
@@ -3,9 +3,6 @@ using namespace std;
 int main()
 {
 	cout << "Hello World!\n";
-	cout<<"Changes from Github\n";
-	cout<<"Changes for Merging\n";
-	cout<<"Changes Made to View Fetch\n";
-	cout<<"Changes Made to View Fetch\n";
+	cout << "Master Branch\n";
 	return 0;
 }
