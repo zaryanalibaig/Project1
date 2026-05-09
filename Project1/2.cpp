2bd0c60c729ca01df8c364fa764cbe71caa762e6
Zaryan <zaryan.ali.baig@gmail.com>
2026-05-09 15:38:43 +0500

    Branch br2 changes

---
+++ Project1/2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Branch br2\n";
+	return 0;
+}
--- Project1/pr.cpp
+++ Project1/pr.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main()
 {
 	cout << "Master Branch\n";
+	cout << "changes from branch br2\n";
 	return 0;
 }
